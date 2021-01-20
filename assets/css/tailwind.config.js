030d5aeb268daf4d374fac4ccbbb10465cd2a885
Tom Tom <70383056+pajakbalikpapan@users.noreply.github.com>
2021-01-20 21:11:54 +0800

    rebuilding site Wed, Jan 20, 2021  9:11:54 PM

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,9 +1,24 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
+  important: true,
   theme: {
     extend: {
-      textColor: ['active']
+      textColor: ['active'],
+      colors: {
+        blue: {
+          '450': '#5F99F7'
+        },
+        red: {
+          '450': '#fa243c'
+        },
+        yellow: {
+          '150': 'FAF2E3'
+        },
+        green: {
+          '1000': '22cc52'
+        }
+      }
     }
   },
   variants: {},
---
+++ content/featured.md
@@ -0,0 +1,6 @@
++++
+draft= false
+title = "Featured"
+description = "kantor pajak konsultan pajak pelayanan pajak djponline"
+layout = "featured"
++++
